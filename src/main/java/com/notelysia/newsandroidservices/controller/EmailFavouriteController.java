c13d75b4a1e517bb3cacafd0a42daf9b20d29ca6
2dgirlismywaifu <ngothanhlong19943@gmail.com>
2023-02-12 01:24:25 +0700

    update services

--- src/main/java/com/notelysia/newsandroidservices/controller/EmailFavouriteController.java
+++ src/main/java/com/notelysia/newsandroidservices/controller/EmailFavouriteController.java
@@ -52,22 +52,23 @@ public class EmailFavouriteController {
 
     //insert news favourite
     //services will use params: user_id, url, title, image_url, source_name
-    @RequestMapping(value = "/account/favourite/news/add", params = {"userid", "url", "title", "imageurl", "sourcename"}, method = RequestMethod.POST)
+    @RequestMapping(value = "/account/favourite/news/add", params = {"userid", "url", "title", "imageurl", "pubdate", "sourcename"}, method = RequestMethod.POST)
     public ResponseEntity<HashMap<String, String>> userNewsFavourite (
             @RequestParam(value = "userid") String user_id, @RequestParam(value = "url") String url,
             @RequestParam(value = "title") String title, @RequestParam(value = "imageurl") String image_url,
-            @RequestParam(value = "sourcename") String source_name) {
+            @RequestParam(value = "pubdate") String pub_date, @RequestParam(value = "sourcename") String source_name) {
         con = new AzureSQLConnection().getConnection();
         String favourite_id_random = new RandomNumber().generateSSONumber();
         HashMap<String, String> respond = new HashMap<>();
         try {
-            ps = con.prepareStatement("INSERT INTO SYNC_NEWS_FAVOURITE (favourite_id, user_id, url, title, image_url, source_name) VALUES (?, ?, ?, ?, ?, ?)");
+            ps = con.prepareStatement("INSERT INTO SYNC_NEWS_FAVOURITE (favourite_id, user_id, url, title, image_url, pubdate, source_name) VALUES (?, ?, ?, ?, ?, ?, ?)");
             ps.setString(1, favourite_id_random);
             ps.setString(2, user_id);
             ps.setString(3, url);
             ps.setString(4, title);
             ps.setString(5, image_url);
-            ps.setString(6, source_name);
+            ps.setString(6, pub_date);
+            ps.setString(7, source_name);
             int result = ps.executeUpdate();
             if (result > 0) {
                 respond.put("sync_id", favourite_id_random);
@@ -164,6 +165,7 @@ public class EmailFavouriteController {
                 sourceFavourite.setUrl(rs.getString("url"));
                 sourceFavourite.setTitle(rs.getString("title"));
                 sourceFavourite.setImage_url(rs.getString("image_url"));
+                sourceFavourite.setPub_date(rs.getString("pubdate"));
                 sourceFavourite.setSource_name(rs.getString("source_name"));
                 sourceFavourites.add(sourceFavourite);
             }
--- src/main/java/com/notelysia/newsandroidservices/controller/SSOFavouriteController.java
+++ src/main/java/com/notelysia/newsandroidservices/controller/SSOFavouriteController.java
@@ -52,22 +52,23 @@ public class SSOFavouriteController {
 
     //insert news favourite
     //services will use params: user_id, url, title, image_url, source_name
-    @RequestMapping(value = "/sso/favourite/news/add", params = {"userid", "url", "title", "imageurl", "sourcename"}, method = RequestMethod.POST)
+    @RequestMapping(value = "/sso/favourite/news/add", params = {"userid", "url", "title", "imageurl", "pubdate","sourcename"}, method = RequestMethod.POST)
     public ResponseEntity<HashMap<String, String>> ssoNewsFavourite (
             @RequestParam(value = "userid") String user_id, @RequestParam(value = "url") String url,
             @RequestParam(value = "title") String title, @RequestParam(value = "imageurl") String image_url,
-            @RequestParam(value = "sourcename") String source_name) {
+            @RequestParam(value = "pubdate") String pub_date, @RequestParam(value = "sourcename") String source_name) {
         con = new AzureSQLConnection().getConnection();
         String favourite_id_random = new RandomNumber().generateSSONumber();
         HashMap<String, String> respond = new HashMap<>();
         try {
-            ps = con.prepareStatement("INSERT INTO SYNC_NEWS_FAVOURITE_SSO (favourite_id, user_id, url, title, image_url, source_name) VALUES (?, ?, ?, ?, ?, ?)");
+            ps = con.prepareStatement("INSERT INTO SYNC_NEWS_FAVOURITE_SSO (favourite_id, user_id, url, title, image_url, pubdate, source_name) VALUES (?, ?, ?, ?, ?, ?, ?)");
             ps.setString(1, favourite_id_random);
             ps.setString(2, user_id);
             ps.setString(3, url);
             ps.setString(4, title);
             ps.setString(5, image_url);
-            ps.setString(6, source_name);
+            ps.setString(6, pub_date);
+            ps.setString(7, source_name);
             int result = ps.executeUpdate();
             if (result > 0) {
                 respond.put("sync_id", favourite_id_random);
@@ -76,6 +77,7 @@ public class SSOFavouriteController {
                 respond.put("title", title);
                 respond.put("image_url", image_url);
                 respond.put("source_name", source_name);
+                respond.put("pubdate", pub_date);
                 respond.put("status", "success");
             } else {
                 respond.put("status", "fail");
@@ -164,6 +166,7 @@ public class SSOFavouriteController {
                 sourceFavourite.setUrl(rs.getString("url"));
                 sourceFavourite.setTitle(rs.getString("title"));
                 sourceFavourite.setImage_url(rs.getString("image_url"));
+                sourceFavourite.setPub_date(rs.getString("pubdate"));
                 sourceFavourite.setSource_name(rs.getString("source_name"));
                 sourceFavourites.add(sourceFavourite);
             }
